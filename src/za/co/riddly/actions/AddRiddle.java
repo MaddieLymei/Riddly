6e6bb49d16ca6cc22c277fd76ac8fbd4dff7cae7
MaddieLymei <97503724+MaddieLymei@users.noreply.github.com>
2022-06-07 23:00:38 +0200

    Design Pattern - command and actions

--- src/za/co/riddly/actions/AddRiddle.java
+++ src/za/co/riddly/actions/AddRiddle.java
@@ -1,5 +1,13 @@
 package za.co.riddly.actions;
-
-public class AddRiddle {
-
+import za.co.riddly.interfaces.RiddleActions;
+import za.co.riddly.models.Riddles;
+public class AddRiddle implements RiddleActions {
+    private Riddles riddles;
+    public AddRiddle(Riddles riddles) {
+     this.riddles = riddles;
+    }
+    @Override
+    public void execute() {
+        riddles.add();
+    }
 }
